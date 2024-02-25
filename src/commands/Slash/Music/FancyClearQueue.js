10e6b3fee99d4d0ac28bc70b73378432d5f8cdec
tpgajay <158246780+tpgajay@users.noreply.github.com>
2024-02-25 13:19:23 +0530

    Update FancyClearQueue.js

--- src/commands/Slash/Music/FancyClearQueue.js
+++ src/commands/Slash/Music/FancyClearQueue.js
@@ -23,7 +23,7 @@ module.exports = {
         const player = client.poru.players.get(interaction.guild.id);
 
         if (!player.queue.length) {
-            const embed = new EmbedBuilder().setColor(client.color).setDescription(`\ <:icon_cross:1200797307805892651>\ | Queue is: \`Empty\``);
+            const embed = new EmbedBuilder().setColor(client.color).setDescription(`\ <a:crosss:1210629485309730907>\ | Queue is: \`Empty\``);
 
             return interaction.editReply({ embeds: [embed] });
         } else {
@@ -31,7 +31,7 @@ module.exports = {
 
             await player.queue.clear();
 
-            const embed = new EmbedBuilder().setColor(client.color).setDescription(`<:Check:1200809259928129547> | \`${length}\` Queue has been: \`Cleared\``);
+            const embed = new EmbedBuilder().setColor(client.color).setDescription(`<a:check:1210630091441180783>| \`${length}\` Queue has been: \`Cleared\``);
 
             return interaction.editReply({ embeds: [embed] });
         }
