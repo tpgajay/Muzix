5e286e32970c71f2b334d4959316de29fdfb9eb9
tpgajay <158246780+tpgajay@users.noreply.github.com>
2024-02-25 13:14:21 +0530

    Update FancyNightcore.js

--- src/commands/Slash/Filters/FancyNightcore.js
+++ src/commands/Slash/Filters/FancyNightcore.js
@@ -24,7 +24,7 @@ module.exports = {
 
         await player.filters.setNightcore(true);
 
-        const embed = new EmbedBuilder().setDescription(`\<a:filter:1200818160828420130>\ | Filter has been set to: \`Nightcore\``).setColor(client.color);
+        const embed = new EmbedBuilder().setDescription(`\<a:filter:1210779838269362237>\ | Filter has been set to: \`Nightcore\``).setColor(client.color);
 
 
         return interaction.editReply({ embeds: [embed] });
