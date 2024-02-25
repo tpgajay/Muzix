ec951e4db36cccada5d7da8126a25c8485bbf7b8
tpgajay <158246780+tpgajay@users.noreply.github.com>
2024-02-25 13:13:48 +0530

    Update FancyEarRape.js

--- src/commands/Slash/Filters/FancyEarRape.js
+++ src/commands/Slash/Filters/FancyEarRape.js
@@ -25,7 +25,7 @@ module.exports = {
 
         await player.setVolume(500);
 
-        const embed = new EmbedBuilder().setDescription(`\<a:filter:1200818160828420130>\ | Filter has been set to: \`EarRape\``).setColor(client.color);
+        const embed = new EmbedBuilder().setDescription(`\<a:filter:1210779838269362237>\ | Filter has been set to: \`EarRape\``).setColor(client.color);
 
      
         return interaction.editReply({ embeds: [embed] });
