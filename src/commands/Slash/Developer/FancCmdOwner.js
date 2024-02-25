cf3c74188565299679c916bf1989b9b60c39c346
tpgajay <158246780+tpgajay@users.noreply.github.com>
2024-02-25 13:10:18 +0530

    Update FancCmdOwner.js

--- src/commands/Slash/Developer/FancCmdOwner.js
+++ src/commands/Slash/Developer/FancCmdOwner.js
@@ -23,33 +23,33 @@ module.exports = {
 
     // Define premium and developer commands for better readability
     const premiumCommands = [
-      "f?generate : Generate premium user code.",
-      "f?unpremium : Delete user from premium.",
-      "f?list : Get list of all premium users.",
+      "M!generate : Generate premium user code.",
+      "M!unpremium : Delete user from premium.",
+      "M!list : Get list of all premium users.",
     ].join("\n");
 
     const developerCommands = [
-      "f?ban : Ban a user from using the bot.",
-      "f?maintenance : Enable maintenance mode.",
-      "f?eval : Bot evaluation.",
+      "M!ban : Ban a user from using the bot.",
+      "M!maintenance : Enable maintenance mode.",
+      "M!eval : Bot evaluation.",
     ].join("\n");
 
     // Build the embed with a more professional and organized structure
     const embed = new EmbedBuilder()
       .setTitle("Owner Commands")
       .setDescription(`
-        <:Premium:1200817621503840276> **Premium Commands**
+        <:emoji_24:1210633582905655296> **Premium Commands**
         \`\`\`yaml
         ${premiumCommands}
         \`\`\`
 
-        <:developer:1200817682971373783> **Developer Commands**
+        <a:Developer:1210633774086488155> **Developer Commands**
         \`\`\`yaml
         ${developerCommands}
         \`\`\`
       `)
       .setColor(client.color)
-      .setURL("https://discord.gg/7MEHnM6T87"); // Replace with your actual support server URL
+      .setURL("https://discord.com/invite/ByeTFze94A"); // Replace with your actual support server URL
 
     // Send the edited reply with the embed
     return interaction.editReply({ embeds: [embed] });
